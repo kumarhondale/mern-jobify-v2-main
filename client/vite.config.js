07a1312f5b4c9f856ab6c3193659bd9f8b6282b7
Rajkumar hondale <rajkumarhondale@Rajkumars-Mac-mini.local>
2023-09-14 18:19:25 +0530

    client init

---
+++ client/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 4000,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:3000/api",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
